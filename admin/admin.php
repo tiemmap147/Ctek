d0cbef4e2dd688f95913e8f7ad5fda3e282fdc2c
Khoa Ngô <khoango@Khoas-MacBook-Pro.local>
2021-05-17 23:21:25 +0700

    KET NOI SQL

---
+++ admin/admin.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title>
+        Quản Lý Ctek
+    </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styleadmin.css">
+</head>
+<body>
+    <div class="menu-left">
+    <a href="index.php" class="header-logo">
+        <img src="../images/cteck1-01.png" class="header-logo-img">
+    </a>
+
+    <ul>
+        <li class="admin-menu-item">
+            <a href="#" class="admin-menu-link">DATA</a>
+        </li>
+
+        <li class="admin-menu-item">
+            <a href="#" class="admin-menu-link">Thêm dữ liệu</a>
+        </li>
+
+        <li class="admin-menu-item">
+            <a href="#" class="admin-menu-link">Xoá dữ liệu</a>
+        </li>
+
+        <li class="admin-menu-item">
+            <a href="#" class="admin-menu-link">Chỉnh sửa</a>
+        </li>
+    </ul>
+    </div>
+    <div class="admin-content">
+
+    </div>
+</body>
+</html>
