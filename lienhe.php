9d98a50260d7ce20ac20b711b40bb4fe1d89cc1b
nguyensonzzz <83354046+nguyensonzzz@users.noreply.github.com>
2021-05-18 23:29:26 +0700

    lien he

--- lienhe.php
+++ lienhe.php
@@ -27,9 +27,9 @@
     </div>
     
     <div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6732868266167!2d106.69611331411632!3d10.759643262447101!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f131f35179b%3A0xd01df0d07a1bbb14!2zMjYxIMSQxrDhu51uZyBLaMOhbmggSOG7mWksIFBoxrDhu51uZyA1LCBRdeG6rW4gNCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1621331508220!5m2!1svi!2s"
-         width="1400" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1959.396418166867!2d106.6979606078376!3d10.827159847605024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528f4a62fce9b%3A0xc99902aa1e26ef02!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBWxINuIExhbmcgLSBDxqEgc-G7nyAz!5e0!3m2!1svi!2s!4v1621355126930!5m2!1svi!2s"
+          width="1400" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     </div>
 
 
