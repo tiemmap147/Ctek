6e0d485c7fe82087d108a9690c46a58d2e272f95
xuangiau <dxgiau0203@gmail.com>
2021-05-18 17:03:45 +0700

    quy su

--- detail.php
+++ detail.php
@@ -17,7 +17,7 @@
 	</header>
     <center>
     <div class="div-detail">
-        <div class="detail"></div>
+            <div class="detail"></div>
             <div class="name"><h1>name</h1></div>
             <div class="price"><h2>Giá</h2></div>
             <div class="dc">Mô tả sản phẩm</div>
@@ -25,45 +25,7 @@
                 <div class="order"><a href="" class= "order_add">Mua ngay</a> </div>
                 <div class ="addbox"><a href="" class= "order_add">Thêm vào giỏ hàng</a>
             </div>
-        </div>
     </div>
-    <div class="splq"><h2>Sản Phẩm liên quan</h2></div>
-        <div class="grid">
-
-            <a href="#" class="griditem grid_item2">
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-            </a>
-            <a href="#" class="griditem grid_item3">
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-                
-            </a>
-            <a href="#" class="griditem grid_item4">
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-            </a>
-            <a href="#" class="griditem grid_item5">
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-            </a>
-            <a href="#" class="griditem grid_item6">
-
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-            </a>
-            <a href="#" class="griditem grid_item7">
-
-                <div class="griditem_images"></div>
-                <div class="griditem_label"></div>
-                <div class="griditem_gia"></div>
-            </a>
-        </div>
         
             
    
