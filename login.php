1e8aaae308665fda55a4321007c1d4dc6ee421a2
xuangiau <dxgiau0203@gmail.com>
2021-05-19 12:39:06 +0700

    cssloginsignup

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="CSS/stylecontent.css">
+    <link rel="stylesheet" type="text/css" href="CSS/stylelogin.css">
     <link rel="shortcut icon" type="image/png" href="images/cteck1-01.png"/>
 </head>
 <body>
